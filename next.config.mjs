817c719aff961c75c66560ab9216ee911f41ed84
Arian <Arianbod@outlook.com>
2025-01-30 00:47:44 +0300

    cors policies edited ver 2.9 build 1.1.7 - stable

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Allow CORS for all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000, http://localhost:3001, https://lauria-hill.com" }, // Added example.com
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ];
+    },
     images: {
         remotePatterns: [
             {
@@ -18,4 +32,4 @@ const nextConfig = {
     },
 };
 
-export default nextConfig
+export default nextConfig;
